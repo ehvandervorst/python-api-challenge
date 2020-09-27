5d12ef7975ed6b2e8ec7d2610854d1d6130d4234
Herrity <eherrity@wm.edu>
2020-09-27 14:46:27 -0700

    setting up API calls

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "86e99f8f52b689a868c3db258b66be67"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDKG9sVsCjH-npjAEqS5ndEvtuEoPhqbJ0"
